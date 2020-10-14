f130929525ab732faeed730a20b2ff5e9c6fcf96
madhav06 <mnandan06@gmail.com>
2020-10-14 20:55:46 +0530

    max. product of triplet in array

--- InterviewCake_Problems/Triplet_Products.py
+++ InterviewCake_Problems/Triplet_Products.py
@@ -6,7 +6,35 @@
 
 # Output: 1200
 
-import sys
 
 # function to find a maximum product of a triplet in array of integers of size n
+
+
+def maxProduct(arr, n): 
+ 
+    # if size is less than 3, no triplet exists 
+    if n < 3:
+        return -1
+ 
+    # Sort the array in ascending order 
+    arr.sort()
+ 
+    # Return the maximum of product of last 
+    # three elements and product of first 
+    # two elements and last element 
+    return max(arr[0] * arr[1] * arr[n - 1], 
+               arr[n - 1] * arr[n - 2] * arr[n - 3]) 
+ 
+# Driver Code
+if __name__ == "__main__":
+ 
+    arr = [-10, -3, 5, 6, -20] 
+    n = len(arr) 
+ 
+    _max = maxProduct(arr, n) 
+ 
+    if _max == -1: 
+        print("No Triplet Exists") 
+    else:
+        print("Maximum product is", _max) 
 
