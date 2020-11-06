bac9887d0fe5e5f8a08e87e5f6c5f43caf9f634c
madhav06 <mnandan06@gmail.com>
2020-11-07 00:02:45 +0530

    Busiest Time in Mall

--- InterviewBit_Problems/pramp_ques/Solution_Box/Busiest_TimeIn_Mall.py
+++ InterviewBit_Problems/pramp_ques/Solution_Box/Busiest_TimeIn_Mall.py
@@ -6,9 +6,24 @@ def find_busiest_period(data):
 
 	people , max_time, max_people = 0,0,0
 
-	for j in range(len(data)):
-		# set i 
-		i = data[j]
 
-		if i[2] == 1:
-			people += i[1]
+    # ITERATEING thru indexes of nested array
+	for i in range(len(data)):
+		# set j to  value of i
+		j = data[i]
+        
+        # if third ele == 1, add index 1 to people
+		if j[2] == 1:
+			people += j[1]
+		else:
+			people -= j[1]
+
+		if i == len(data)-1 or data[i][0] != data[i+1][0]:
+
+			if people > max_people:
+
+				max_people = people
+
+				max_time = j[0]
+
+return max_time
