81b3b22d2c4c866c9c86fc6313aa45cb73e3b4dd
madhav06 <mnandan06@gmail.com>
2020-11-07 21:35:46 +0530

    Prime Sum Program

--- InterviewBit_Problems/math/PrimeSum.py
+++ InterviewBit_Problems/math/PrimeSum.py
@@ -1,6 +1,7 @@
+# Python3
+
 # Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to given number.
 
-# NOTE A solution will always exist. read Goldbach’s conjecture
 
 # Example:
 
@@ -15,20 +16,21 @@
 
 # [a, b] < [c, d] 
 
-# If a < c OR a==c AND b < d.
+# If a < c OR a==c AND b < d
 
 import math
 class Solution:
-    def isPrime(self,n):
-        for i in range(2,int(math.sqrt(n))+1):
-            if n%i==0:
-                return False
-        return True
-    # @param A : integer
-    # @return a list of integers
-    def primesum(self, A):
-        for i in range(2,(A//2)+1):
-            # che cking with AND Op
-            if self.isPrime(i) and self.isPrime(A-i):
-                return (i,A-i)
-        return (0,0)
+	def isPrime(self, n):
+		for i in range(2, int(math.sqrt(n)) + 1):
+			if n%i == 0:
+				return False
+		return True
+
+	# @param A: integer
+	# @return a list of integers
+
+	def primesum(self, A):
+		for i in range(2, (A//2) + 1):
+			if self.isPrime(i) and self.isPrime(A-i):
+				return (i, A-i)
+		return (0,0)
