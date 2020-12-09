fc88ffc9e7be4166645c7282cabc1e5b26b5d3bd
madhav06 <mnandan06@gmail.com>
2020-12-09 20:08:19 +0530

    Print table program

---
+++ InterviewCake_Problems/Print_Table.py
@@ -0,0 +1,8 @@
+# Python3
+# Python Program to print table of a given number:
+
+n = int(input("Enter the number to print the tables for: "))
+
+for i in range(1, 11):
+    print(n, "x", i, "=", n*i)
+    
