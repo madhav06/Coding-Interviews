5907435a1795ee59f35f20723672d3a9968553bd
madhav06 <mnandan06@gmail.com>
2020-12-10 11:42:12 +0530

    Count of vowels in string

---
+++ InterviewCake_Problems/Vowels_In_String.py
@@ -0,0 +1,10 @@
+# python3
+# python program to count the number of vowels in string:
+
+string = raw_input("Enter string: ")
+vowels = 0
+for i in string:
+    if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+        vowels = vowels + 1
+print("Number of vowels are: ")
+print(vowels)
