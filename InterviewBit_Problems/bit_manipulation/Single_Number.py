62f4c0ae87607d3a84e0f66b981508c199266b4b
madhav06 <mnandan06@gmail.com>
2021-04-15 22:00:49 +0530

    Single Occurance of Num

---
+++ InterviewBit_Problems/bit_manipulation/Single_Number.py
@@ -0,0 +1,38 @@
+# python 3 code
+'''
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Input Format:
+
+    First and only argument of input contains an integer array A
+Output Format:
+
+    return a single integer denoting single element
+Constraints:
+
+2 <= N <= 2 000 000  
+0 <= A[i] <= INT_MAX
+
+Example Input 1:
+    A = [1, 2, 2, 3, 1]
+Example Output 1:
+    3
+Explanation:
+    3 occurs only once
+Example Input 2:
+    A = [1, 2, 2]
+Example Output 2:
+    1
+'''
+
+class Solution:
+    # @param A: tuple of integers
+    # @return an integer
+    def singleNumber(self, A):
+        x = 0
+
+        for n in A:
+            x = x ^ n
+        return x
