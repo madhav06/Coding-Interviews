df3ead1c449b87a7b30ad25d806f8f86a8d3272a
madhav06 <mnandan06@gmail.com>
2021-04-20 23:25:33 +0530

    Add 1 to numer

--- InterviewBit_Problems/arrays/add1_to_Num.py
+++ InterviewBit_Problems/arrays/add1_to_Num.py
@@ -8,6 +8,7 @@
 
 # If the vector has [1, 2, 3]
 
+
 # the returned vector should be [1, 2, 4]
 
 # as 123 + 1 = 124.
@@ -16,9 +17,11 @@ class Solution:
     # @param A : list of integers
     # @return a list of integers
     def plusOne(self, A):
+       
         if A == [0]:
             return [1]
         l = A[::-1] # reverse the list and store
+        
         l += [0] # increase size by 1
         x = 1
         for i in range(len(l)):
