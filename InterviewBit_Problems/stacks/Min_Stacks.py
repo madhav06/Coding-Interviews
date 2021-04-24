ddb9990b353fe7c39f2c2e907627a3a31aa0c1b4
madhav06 <mnandan06@gmail.com>
2021-04-25 00:21:24 +0530

    Minimum of Stack

---
+++ InterviewBit_Problems/stacks/Min_Stacks.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def isEmpty(self):
+        my_stack.x == []
+
+    def push(self, x):
+        return my_stack.x.append()
+
+    def pop(self):
+        return my_stack.pop()
+
+    def top(self):
+        return my_stack.top()
+
+    def getMin(self, x):
+        if top() == 0:
+            return -1
+        if top != 0:
+            if min(x.append):
+                return x.append
+            return top()
+
+    def __init__(self):
+        my_stack = []
+
