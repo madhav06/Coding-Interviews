38eac9b25f97ebce2bab4fba08069f82f2385d4a
Madhav Nandan <madhavnandan@Madhavs-MacBook-Air.local>
2021-05-04 22:56:49 +0530

    Nearest Smaller element in an array Using Stack, Efficient method

--- InterviewBit_Problems/stacks/Nearest_Smaller_Element.py
+++ InterviewBit_Problems/stacks/Nearest_Smaller_Element.py
@@ -1,31 +1,20 @@
-# Python3 implementation of efficient
-# algorithm to find smaller element
-# on left side
-import math as mt
- 
-# Prints smaller elements on left
-# side of every element
-def printPrevSmaller(arr, n):
- 
-    # Create an empty stack
-    S = list()
- 
-    # Traverse all array elements
-    for i in range(n):
-     
-        # Keep removing top element from S
-        # while the top element is greater
-        # than or equal to arr[i]
-        while (len(S) > 0 and S[-1] >= arr[i]):
-            S.pop()
- 
-        # If all elements in S were greater
-        # than arr[i]
-        if (len(S) == 0):
-            print("_, ", end = "")
-        else: # Else print the nearest
-              # smaller element
-            print(S[-1], end = ", ")
- 
-        # Push this element
-        S.append(arr[i])
+# python3 Code here
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def prevSmaller(self, array):
+        """ Time complexity: O(n). Space complexity: O(n), n is len(array).
+        """
+        stack = []
+        result = []
+        for num in array:
+            # see of there's integer smaller than num in the stack
+            while stack and stack[-1] >= num:
+                stack.pop()
+            if stack:  # found the smaller integer
+                result.append(stack[-1])
+            else:  # stack is empty, smaller integer wasn't found
+                result.append(-1)
+            stack.append(num)  # push current num to the stack
+        return result
