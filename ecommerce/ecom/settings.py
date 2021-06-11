33c2a9c1b088c63829e7fa2c84c2ae5056364088
madhav06 <mnandan06@gmail.com>
2021-06-11 10:18:12 +0530

    Updating README

--- ecommerce/ecom/settings.py
+++ ecommerce/ecom/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog',
-    'crispy_forms',
+    'catalog.apps.CatalogConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
