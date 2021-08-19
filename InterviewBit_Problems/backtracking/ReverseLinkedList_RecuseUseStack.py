1f167f6a13690269e1e36c258d13f33c63cfa3d1
madhav06 <mnandan06@gmail.com>
2021-08-19 23:51:22 +0530

    Reverse LinkedList Recursion Uisng Stack

---
+++ InterviewBit_Problems/backtracking/ReverseLinkedList_RecuseUseStack.py
@@ -0,0 +1,45 @@
+# Reverse Linked List Recursion Using Stack
+
+# Python code for the above approach
+
+# Definition for singly-linked list.
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+
+class Solution:
+
+	# Program to reverse the linked list
+	# using stack
+	def reverseLLUsingStack(self, head):
+		
+		# Initialise the variables
+		stack, temp = [], head
+		
+		while temp:
+			stack.append(temp)
+			temp = temp.next
+
+		head = temp = stack.pop()
+		
+		# Untill stack is not
+		# empty
+		while len(stack) > 0:
+			elem = stack.pop()
+			temp.next = elem
+			temp = elem
+
+		elem.next = None
+		return head
+
+
+# if __name__ == "__main__":
+# 	head = ListNode(1, ListNode(2, ListNode(3,
+# 						ListNode(4, ListNode(5)))))
+# 	obj = Solution()
+# 	head = obj.reverseLLUsingStack(head)
+# 	while head:
+# 		print(head.val, end=' ')
+# 		head = head.next
